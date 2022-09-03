1063fa826608c7e90bb6db113e95ad204ec2d5f5
Prathap Reddy <kprathapreddy1997@gmail.com>
2022-09-03 11:11:25 -0400

    added code to use redux dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { rootReducer } from './rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware()));
 export default store;
